0c192080a850a153bd7183ea7ea31bc4915586c4
INTI-0361 <INTI-0361@DESKTOP-L9V3D4E>
2017-06-13 11:33:22 +0200

    Web Update

--- Newman-Immo/src/main/webapp/assets/js/js-perso/routeProvider.js
+++ Newman-Immo/src/main/webapp/assets/js/js-perso/routeProvider.js
@@ -1,1 +1,8 @@
-
+monApp.config(function($routeProvider){
+   $routeProvider.when('/home',{controller:'ctrlHome',templateUrl:'assets/templates/home.html'})
+                 .when('/conseiller/one',{controller:'ctrlOneConseiller',templateUrl:'assets/templates/conseiller/getone.html'})
+                 .when('/conseiller/all',{controller:'ctrlAllConseiller',templateUrl:'assets/templates/conseiller/getall.html'})
+                 .when('/conseiller/add',{controller:'ctrlAddConseiller',templateUrl:'assets/templates/conseiller/add.html'})
+                 .when('/conseiller/update',{controller:'ctrlUpdateConseiller',templateUrl:'assets/templates/conseiller/update.html'})
+                 .otherwise({redirectTo:'/home'});
+	});
