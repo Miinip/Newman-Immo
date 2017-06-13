85a9633a8d43f2d3012bf6885eb4e9f4fd67979d
INTI-0362 <INTI-0362@DESKTOP-L0FU12G>
2017-06-13 16:32:53 +0200

    ENFIN bis

--- Newman-Immo/src/main/java/fr/adaming/model/Conseiller.java
+++ Newman-Immo/src/main/java/fr/adaming/model/Conseiller.java
@@ -2,8 +2,10 @@ package fr.adaming.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
