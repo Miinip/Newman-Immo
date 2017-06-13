a0220a58f428839472161c8ca4d64d73825117c2
INTI-0367 <INTI-0367@DESKTOP-0UOKIP4>
2017-06-13 12:44:40 +0200

    VisiteRestController OK

--- Newman-Immo/src/main/java/fr/adaming/rest/VisiteRestController.java
+++ Newman-Immo/src/main/java/fr/adaming/rest/VisiteRestController.java
@@ -1,8 +1,11 @@
 package fr.adaming.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,5 +37,42 @@ public class VisiteRestController {
 			return new ResponseEntity(HttpStatus.BAD_REQUEST);
 		}
 	}
+	
+	@RequestMapping(value="/update",method=RequestMethod.PUT,consumes="application/json")
+	public ResponseEntity updateVisiteRest(@RequestBody Visite v) {
+		int status = visiteService.updateVisite(v);
+		if(status == 1){
+			System.out.println("-------- updateVisiteRest [OK] --------");
+			return new ResponseEntity(HttpStatus.OK);
+		}else{
+			System.out.println("-------- updateVisiteRest [BAD_REQUEST] --------");
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@RequestMapping(value="/delete/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity deleteVisiteRest(@PathVariable("id") int id) {
+		int status = visiteService.deleteVisite(id);
+		if(status == 1){
+			System.out.println("-------- deleteVisiteRest [OK] --------");
+			return new ResponseEntity(HttpStatus.OK);
+		}else{
+			System.out.println("-------- deleteVisiteRest [BAD_REQUEST] --------");
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	
+	
+	@RequestMapping(value="/all",method=RequestMethod.GET,produces="application/json")
+	public List<Visite> getAllVisiteRest() {
+		return visiteService.getAllVisite();
+	}
+	
+	@RequestMapping(value="/one/{id}",method=RequestMethod.GET,produces="application/json")
+	public Visite getByIdVisiteRest(@PathVariable("id") int id) {
+		return visiteService.getByIdVisite(id);
+	}
+	
 
 }
