cf366771d6001631a0e88c406ae1ff13c9e91e2b
INTI-0363 <INTI-0363@DESKTOP-I501RGH>
2017-06-13 12:00:37 +0200

    Mise en place de try catch pour les méthode de
    propriétaire et service rest pour add

--- Newman-Immo/src/main/java/fr/adaming/dao/ProprietaireDaoImpl.java
+++ Newman-Immo/src/main/java/fr/adaming/dao/ProprietaireDaoImpl.java
@@ -17,9 +17,10 @@ public class ProprietaireDaoImpl implements IProprietaireDao {
 
 	@Autowired
 	private SessionFactory sf;
-	
+
 	/**
-	 * @param sf the sf to set
+	 * @param sf
+	 *            the sf to set
 	 */
 	public void setSf(SessionFactory sf) {
 		this.sf = sf;
@@ -27,28 +28,40 @@ public class ProprietaireDaoImpl implements IProprietaireDao {
 
 	public int createProprietaire(Proprietaire prop) {
 		Session s = sf.getCurrentSession();
-		s.save(prop);
-		return 1;
+		try {
+			s.save(prop);
+			return 1;
+		} catch (Exception ex) {
+			return 0;
+		}
 	}
 
 	public int updateProprietaire(Proprietaire prop) {
 		Session s = sf.getCurrentSession();
-		Proprietaire prop_rec = (Proprietaire) s.get(Proprietaire.class, prop.getId());
-		prop_rec.setNom(prop.getNom());
-		prop_rec.setPrenom(prop.getPrenom());
-		prop_rec.setTelPerso(prop.getTelPerso());
-		prop_rec.setTelPro(prop.getTelPro());
-		prop_rec.setAdresse(prop.getAdresse());
-		prop_rec.setListeBien(prop.getListeBien());
-		s.merge(prop_rec);
-		return 1;
+		try {
+			Proprietaire prop_rec = (Proprietaire) s.get(Proprietaire.class, prop.getId());
+			prop_rec.setNom(prop.getNom());
+			prop_rec.setPrenom(prop.getPrenom());
+			prop_rec.setTelPerso(prop.getTelPerso());
+			prop_rec.setTelPro(prop.getTelPro());
+			prop_rec.setAdresse(prop.getAdresse());
+			prop_rec.setListeBien(prop.getListeBien());
+			s.merge(prop_rec);
+			return 1;
+		} catch (Exception ex) {
+			return 0;
+		}
 	}
 
 	public int deleteProprietaire(int id) {
 		Session s = sf.getCurrentSession();
-		Proprietaire prop =(Proprietaire) s.get(Proprietaire.class, id);
-		s.delete(prop);
-		return 1;
+		try {
+			Proprietaire prop = (Proprietaire) s.get(Proprietaire.class, id);
+			s.delete(prop);
+			return 1;
+		} catch (Exception ex) {
+			return 0;
+		}
 	}
 
 	public Proprietaire getOnePropbyId(Proprietaire prop) {
@@ -65,7 +78,8 @@ public class ProprietaireDaoImpl implements IProprietaireDao {
 
 	public int attributePropToConseiller(Proprietaire prop, BienImmobilier bienImmo) {
 		Session s = sf.getCurrentSession();
-		Query query = s.createQuery("UPDATE Proprietaire prop SET prop.id=:pIdProp WHERE bienImmo.id=:pIdBienImmobilier");
+		Query query = s
+				.createQuery("UPDATE Proprietaire prop SET prop.id=:pIdProp WHERE bienImmo.id=:pIdBienImmobilier");
 		query.setParameter("pIdProp", prop.getId());
 		query.setParameter("pIdBienImmobilier", bienImmo.getId());
 		query.executeUpdate();
--- Newman-Immo/src/main/java/fr/adaming/rest/ProprietaireRestController.java
+++ Newman-Immo/src/main/java/fr/adaming/rest/ProprietaireRestController.java
@@ -1,10 +1,17 @@
 package fr.adaming.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.adaming.model.Proprietaire;
 import fr.adaming.service.IProprietaireService;
 
 @RestController
+@RequestMapping("/proprietaire")
 public class ProprietaireRestController {
 
 	private IProprietaireService propService;
@@ -17,6 +24,16 @@ public class ProprietaireRestController {
 	}
 	
 
-	
+	@RequestMapping(value="/add",method=RequestMethod.POST,consumes="application/json")
+	public ResponseEntity addProprietaireRest (@RequestBody Proprietaire prop){
+		int status = propService.createProprietaire(prop);
+		if (status==1){
+			System.out.println("-------- addProprietaireRest [OK] --------");
+			return new ResponseEntity(HttpStatus.OK);
+		}else{
+			System.out.println("-------- addProprietaireRest [BAD_REQUEST] -------");
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+	}
 	
 }
