10f335fe16b0540858decfd31b6d9bd4586d95a3
INTI-0361 <INTI-0361@DESKTOP-L9V3D4E>
2017-06-13 12:19:36 +0200

    edit web

---
+++ Newman-Immo/src/main/webapp/assets/js/js-perso/controllerConseiller.js
@@ -0,0 +1,107 @@
+monApp.controller("ctrlOneConseiller",function($scope,$rootScope,$location,conseillerProvider){
+	$scope.msg = "One Conseiller";
+	$scope.idToFind =undefined;
+	$scope.find = function(){
+		conseillerProvider.getOneConseiller($scope.idToFind,function(callback){
+			$scope.Conseiller = callback;
+		})
+	}
+	
+	$scope.suprimer = function(){
+		conseillerProvider.doDeleteConseiller($scope.Conseiller.id,function(callback){
+			if(callback == "OK"){
+				$location.path('/conseiller/all');
+			}
+		})
+	}
+	
+	$scope.modifier = function(){
+		$rootScope.conseillerFormRoot = $scope.Conseiller;
+		$location.path('/conseiller/update');
+	}
+});
+
+monApp.controller("ctrlAllConseiller",function($scope,$rootScope,$location,conseillerProvider){
+	$scope.msg = "List of Conseiller";
+	conseillerProvider.getAllConseiller(function(callback){
+		$scope.listeConseillers = callback;
+	})
+	
+	$scope.suprimer = function(idToDelete){
+		conseillerProvider.doDeleteConseiller(idToDelete,function(callback){
+			if(callback == "OK"){
+				conseillerProvider.getAllConseiller(function(callback){
+					$scope.listeConseillers = callback;
+				})
+			}
+		})
+	}
+	
+	$scope.modifier = function(conseillerToUpdate){
+		$rootScope.conseillerFormRoot = conseillerToUpdate;
+		$location.path('/conseiller/update');
+	}
+	
+	$scope.MouseOverRow = {};
+	$scope.doShow = function(id) {
+	    $scope.MouseOverRow[id] = true;
+	}
+	$scope.doHide = function(id) {
+	    $scope.MouseOverRow[id] = false;
+	}
+});
+
+monApp.controller("ctrlAddConseiller",function($scope,$location,conseillerProvider){
+	$scope.msg = "Add Conseiller";
+	$scope.conseillerForm = {	id:undefined,
+			nom:"",
+			prenom:"",
+			login:"",
+			password:"",
+			adresse:{	rue:"",
+						numero:"",
+						codePostal:undefined,
+						localite:"",
+						pays:""
+						}
+			};
+			
+	$scope.ajouter = function(){
+		conseillerProvider.doAddConseiller($scope.conseillerForm,function(callback){
+			if(callback == "OK"){
+				$location.path('/conseiller/all');
+			}
+		})
+	}
+});
+
+monApp.controller("ctrlUpdateConseiller",function($scope,$rootScope,$location,conseillerProvider){
+	$scope.msg = "Update Conseiller";
+	$scope.conseillerForm = {	id:undefined,
+			nom:"",
+			prenom:"",
+			login:"",
+			password:"",
+			adresse:{	rue:"",
+						numero:"",
+						codePostal:undefined,
+						localite:"",
+						pays:""
+						}
+			};
+		
+		if($rootScope.conseillerFormRoot != null){
+			$scope.conseillerForm = $rootScope.conseillerFormRoot;
+			$rootScope.conseillerFormRoot = null;
+		}
+
+
+	
+	$scope.modifier = function(){
+		conseillerProvider.doUpdateConseiller($scope.conseillerForm,function(callback){
+			if(callback == "OK"){
+				$location.path('/conseiller/all');
+			}
+		})
+	}
+});
