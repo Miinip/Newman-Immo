0f173bf4111d0d29ba76b89e82bf68b8dc8c132e
INTI-0362 <INTI-0362@DESKTOP-L0FU12G>
2017-06-13 10:06:44 +0200

    Model BienImmo, Client et Personne (en cours)

--- Newman-Immo/src/main/java/fr/adaming/model/Client.java
+++ Newman-Immo/src/main/java/fr/adaming/model/Client.java
@@ -2,9 +2,26 @@ package fr.adaming.model;
 
 import java.util.List;
 
-public class Client {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="clients")
+public class Client {
+	
+	@OneToMany
 	private List<ClasseStandard> listeVoeux;
+	
+	@OneToMany
 	private List<Visite> listeVisitePrevue;
+	
+	@OneToOne(mappedBy="clientAcquereur")
+	private BienImmobilier bienImmo;
 	
 }
