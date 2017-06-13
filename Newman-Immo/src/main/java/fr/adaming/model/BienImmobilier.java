fba061c1266fa0cb6def26a86b126142dfdf1f4f
INTI-0362 <INTI-0362@DESKTOP-L0FU12G>
2017-06-13 10:24:51 +0200

    Model BienImmo

--- Newman-Immo/src/main/java/fr/adaming/model/BienImmobilier.java
+++ Newman-Immo/src/main/java/fr/adaming/model/BienImmobilier.java
@@ -1,5 +1,6 @@
 package fr.adaming.model;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -68,5 +69,151 @@ public class BienImmobilier {
 	@ManyToOne
 	@JoinColumn(name="proprio_id", referencedColumnName="id")
 	private Proprietaire proprietaire;
+
+	// Constructeur vide
+	public BienImmobilier() {
+		super();
+	}
+
+	// Constructeur param - id
+	public BienImmobilier(byte[] photo, String statut, Date dateSoumission, Date dateMiseDispo, double revenuCadastral,
+			Adresse localisation) {
+		super();
+		this.photo = photo;
+		this.statut = statut;
+		this.dateSoumission = dateSoumission;
+		this.dateMiseDispo = dateMiseDispo;
+		this.revenuCadastral = revenuCadastral;
+		this.localisation = localisation;
+	}
+
+	// Constructeur param + id
+	public BienImmobilier(int id, byte[] photo, String statut, Date dateSoumission, Date dateMiseDispo,
+			double revenuCadastral, Adresse localisation) {
+		super();
+		this.id = id;
+		this.photo = photo;
+		this.statut = statut;
+		this.dateSoumission = dateSoumission;
+		this.dateMiseDispo = dateMiseDispo;
+		this.revenuCadastral = revenuCadastral;
+		this.localisation = localisation;
+	}
+
+	// Getters et Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	public String getStatut() {
+		return statut;
+	}
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+
+	public Date getDateSoumission() {
+		return dateSoumission;
+	}
+
+	public void setDateSoumission(Date dateSoumission) {
+		this.dateSoumission = dateSoumission;
+	}
+
+	public Date getDateMiseDispo() {
+		return dateMiseDispo;
+	}
+
+	public void setDateMiseDispo(Date dateMiseDispo) {
+		this.dateMiseDispo = dateMiseDispo;
+	}
+
+	public double getRevenuCadastral() {
+		return revenuCadastral;
+	}
+
+	public void setRevenuCadastral(double revenuCadastral) {
+		this.revenuCadastral = revenuCadastral;
+	}
+
+	public Adresse getLocalisation() {
+		return localisation;
+	}
+
+	public void setLocalisation(Adresse localisation) {
+		this.localisation = localisation;
+	}
+
+	public Client getClientAcquereur() {
+		return clientAcquereur;
+	}
+
+	public void setClientAcquereur(Client clientAcquereur) {
+		this.clientAcquereur = clientAcquereur;
+	}
+
+	public Conseiller getConseiller() {
+		return conseiller;
+	}
+
+	public void setConseiller(Conseiller conseiller) {
+		this.conseiller = conseiller;
+	}
+
+	public List<Visite> getListeVisites() {
+		return listeVisites;
+	}
+
+	public void setListeVisites(List<Visite> listeVisites) {
+		this.listeVisites = listeVisites;
+	}
+
+	public List<Contrat> getListeContrats() {
+		return listeContrats;
+	}
+
+	public void setListeContrats(List<Contrat> listeContrats) {
+		this.listeContrats = listeContrats;
+	}
+
+	public ClasseStandard getClasseStandard() {
+		return classeStandard;
+	}
+
+	public void setClasseStandard(ClasseStandard classeStandard) {
+		this.classeStandard = classeStandard;
+	}
+
+	public Proprietaire getProprietaire() {
+		return proprietaire;
+	}
+
+	public void setProprietaire(Proprietaire proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+
+	@Override
+	public String toString() {
+		return "BienImmobilier [id=" + id + ", photo=" + Arrays.toString(photo) + ", statut=" + statut
+				+ ", dateSoumission=" + dateSoumission + ", dateMiseDispo=" + dateMiseDispo + ", revenuCadastral="
+				+ revenuCadastral + ", localisation=" + localisation + "]";
+	}
+	
+	
+	
+	
 	
 }
